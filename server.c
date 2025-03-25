5e5280c209d830cf543df7927503caae9ed262a4
Augustas <kniskiukas@gmail.com>
2025-03-25 15:05:31 +0200

    change revert, core dumps

--- server.c
+++ server.c
@@ -1,5 +1,8 @@
 
 
+
+// fix the server.c file so that the board is 80 in x and 20 in y and the commands first take x, not y
+
 #ifdef _WIN32
 #include <winsock2.h>
 #define socklen_t int
@@ -27,61 +30,34 @@
 #define BOARD_WIDTH 80
 #define BOARD_HEIGHT 20
 
-struct
-{
+struct {
     int x;
     int y;
     char symbol;
-} DrawPoint; // drawing point struct
+} DrawPoint;
 
-char board[BOARD_HEIGHT][BOARD_WIDTH]; // drawing board array
+char board[BOARD_HEIGHT][BOARD_WIDTH];
 
-// function to draw a point on the board
 int draw(int x, int y, char symbol)
 {
-    y = BOARD_HEIGHT - y - 1;
-
-    if (x < 0 || x >= BOARD_WIDTH || y < 0 || y >= BOARD_HEIGHT) // Check if the coordinates are within bounds
+    if (x < 0 || x >= BOARD_WIDTH || y < 0 || y >= BOARD_HEIGHT)
     {
-        return -1; // Return -1 if the coordinates are invalid
+        return -1;
     }
-    board[y][x] = symbol; // Draw the point
+    board[y][x] = symbol;
     return 0;
 }
 
-// function to convert the board into a string to send to the clients
-char *showBoard()
-{
-    int frame_width = BOARD_WIDTH + 2;
-    int frame_height = BOARD_HEIGHT + 2;
-
-    // Allocate memory for the string
-    char *strboard = (char *)malloc((frame_width * frame_height + 1) * sizeof(char)); // Allocate memory
-    if (strboard == NULL)
-    {
+char* showBoard() {
+    char *strboard = (char*)malloc((BOARD_WIDTH * (BOARD_HEIGHT + 1) + 1) * sizeof(char)); // Allocate memory
+    if (strboard == NULL) {
         perror("Failed to allocate memory for board string");
         return NULL;
     }
-
     int index = 0;
-
-    // Construct the string
-    for (int i = 0; i < frame_height; i++)
-    {
-        for (int j = 0; j < frame_width; j++)
-        {
-            if (i == 0 || i == frame_height - 1)
-            {
-                strboard[index++] = '-'; // Top and bottom frame
-            }
-            else if (j == 0 || j == frame_width - 1)
-            {
-                strboard[index++] = '|'; // Left and right frame
-            }
-            else
-            {
-                strboard[index++] = board[i - 1][j - 1] == 0 ? ' ' : board[i - 1][j - 1]; // Board content
-            }
+    for (int i = 0; i < BOARD_HEIGHT; i++) {
+        for (int j = 0; j < BOARD_WIDTH; j++) {
+            strboard[index++] = board[i][j] == 0 ? ' ' : board[i][j]; // Use space for empty cells
         }
         strboard[index++] = '\n';
     }
@@ -89,16 +65,12 @@ char *showBoard()
     return strboard;
 }
 
-// function to send the board to all clients
-void sendBoardToClients(int current_client_fd, struct pollfd *pfds)
-{
-    char *board_string = showBoard();
-    if (board_string != NULL)
-    {
-        for (int i = 1; i <= MAX_CONNECTED_CLIENTS; i++)
-        {
-            if (pfds[i].fd != -1 && pfds[i].fd != current_client_fd)
-            {
+
+void sendBoardToClients(int current_client_fd, struct pollfd *pfds) {
+    char* board_string = showBoard();
+    if (board_string != NULL) {
+        for (int i = 1; i <= MAX_CONNECTED_CLIENTS; i++) {
+            if (pfds[i].fd != -1 && pfds[i].fd != current_client_fd) {
                 send(pfds[i].fd, board_string, strlen(board_string), 0);
             }
         }
@@ -106,90 +78,68 @@ void sendBoardToClients(int current_client_fd, struct pollfd *pfds)
     }
 }
 
-// function to reset the board
-void resetBoard()
-{
+
+
+
+void resetBoard () {
     memset(board, 0, sizeof(board));
 }
 
-// function to parse the commands
-void commandParse(char *command, int client_fd, struct pollfd *pfds)
-{
+void commandParse (char *command, int client_fd, struct pollfd *pfds) {
     char *token = strtok(command, " ");
-    if (strcmp(token, "/draw") == 0)
-    {
+    if (strcmp(token, "/draw") == 0) {
         token = strtok(NULL, " ");
-        if (token != NULL)
-        {
+        if (token != NULL) {
             DrawPoint.x = atoi(token);
             token = strtok(NULL, " ");
-            if (token != NULL)
-            {
+            if (token != NULL) {
                 DrawPoint.y = atoi(token);
                 token = strtok(NULL, " ");
-                if (token != NULL)
-                {
-                    if (strlen(token) == 1)
-                    {
+                if (token != NULL) {
+                    if (strlen(token) == 1) {
                         DrawPoint.symbol = token[0];
                         int check = draw(DrawPoint.x, DrawPoint.y, DrawPoint.symbol);
-                        if (check == -1)
-                        {
+                        if (check == -1) {
                             send(client_fd, "Invalid coordinates.\n", 21, 0);
-                        }
-                        else
-                        {
+                        } else {
                             sendBoardToClients(-1, pfds);
                             send(client_fd, "Draw successful.\n", 18, 0);
                         }
-                    }
-                    else
-                    {
+                    } else {
                         send(client_fd, "Invalid symbol. Please use a single character.\n", 46, 0);
                     }
-                }
-                else
-                {
+                } else {
                     send(client_fd, "Usage: /draw <x> <y> <symbol>\n", 33, 0);
                 }
-            }
-            else
-            {
+            } else {
                 send(client_fd, "Usage: /draw <x> <y> <symbol>\n", 33, 0);
             }
-        }
-        else
-        {
+        } else {
             send(client_fd, "Usage: /draw <x> <y> <symbol>\n", 33, 0);
         }
-    }
-    else if (strcmp(token, "/show") == 0)
-    {
-        char *board_string = showBoard();
-        if (board_string != NULL)
-        {
+    } else if (strcmp(token, "/show") == 0) {
+        char* board_string = showBoard();
+        if (board_string != NULL) {
             send(client_fd, board_string, strlen(board_string), 0);
             free(board_string);
         }
-    }
-    else if (strcmp(token, "/reset") == 0)
-    {
+    } else if (strcmp(token, "/reset") == 0) {
         resetBoard();
         send(client_fd, "Board reset.\n", 13, 0);
         sendBoardToClients(-1, pfds);
-    }
-    else if (strcmp(token, "/help") == 0)
-    {
+    } else if (strcmp(token, "/help") == 0) {
         send(client_fd, "\nAvailable commands:\n/draw <x> <y> <symbol>\n/show\n/reset\n/help\n/exit\n", 71, 0);
-    }
-    else
-    {
+    } else {
         send(client_fd, "Unknown command. Type /help for a list of available commands.\n", 64, 0);
     }
 }
 
-int main(int argc, char *argv[])
-{
+
+
+
+
+
+int main(int argc, char *argv []){
 #ifdef _WIN32
     WSADATA data;
 #endif
@@ -197,175 +147,166 @@ int main(int argc, char *argv[])
     int l_socket; // socket'as skirtas prisijungimų laukimui
     int c_socket; // prisijungusio kliento socket'as
 
-    struct sockaddr_in servaddr;   // Serverio adreso struktūra
+    struct sockaddr_in servaddr; // Serverio adreso struktūra
     struct sockaddr_in clientaddr; // Prisijungusio kliento adreso struktūra
     socklen_t clientaddrlen = sizeof(struct sockaddr);
 
-    int s_len;         // send length
-    int r_len;         // receive length
-    char buffer[1024]; // buffer for messages
-
-    if (argc != 2)
-    { // tikrinama ar portas nurodytas
+    int s_len;
+    int r_len;
+    char buffer[1024];
+    
+    if (argc != 2){
         printf("USAGE: %s <port>\n", argv[0]);
         exit(1);
     }
 
-    port = atoi(argv[1]); // konvertuojamas portas į skaičių
+    port = atoi(argv[1]);
 
-    if ((port < 1) || (port > 65535))
-    { // tikrinama ar portas yra galiojantis
+    if ((port < 1) || (port > 65535)){
         printf("ERROR #1: invalid port specified.\n");
         exit(1);
     }
 
-    struct pollfd pfds[MAX_CONNECTED_CLIENTS + 1];                  // pollfd array for client connections
-    char usernames[MAX_CONNECTED_CLIENTS + 1][MAX_USERNAME_LENGTH]; // array for storing usernames
-    memset(usernames, 0, sizeof(usernames));                        // initialize usernames array
-    nfds_t nfds = MAX_CONNECTED_CLIENTS;                            // number of file descriptors
-    int max_clients = MAX_CONNECTED_CLIENTS;                        // maximum number of clients
+    struct pollfd pfds[MAX_CONNECTED_CLIENTS + 1];
+    char usernames[MAX_CONNECTED_CLIENTS + 1][MAX_USERNAME_LENGTH];
+    memset(usernames, 0, sizeof(usernames));
+    nfds_t nfds = MAX_CONNECTED_CLIENTS;
 
 #ifdef _WIN32
-    WSAStartup(MAKEWORD(2, 2), &data);
+    WSAStartup(MAKEWORD(2,2),&data);    
 #endif
 
     /*
-     * Sukuriamas serverio socket'as
-     */
-    if ((l_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        fprintf(stderr, "ERROR #2: cannot create listening socket.\n");
+      * Sukuriamas serverio socket'as
+      */
+    if ((l_socket = socket(AF_INET, SOCK_STREAM,0))< 0){
+        fprintf(stderr,"ERROR #2: cannot create listening socket.\n");
+        exit(1);
+    }
+    
+    /*
+      * Išvaloma ir užpildoma serverio adreso struktūra
+      */
+    memset(&servaddr,0, sizeof(servaddr));
+     servaddr.sin_family = AF_INET; // nurodomas protokolas (IP)
+
+    /*
+      * Nurodomas IP adresas, kuriuo bus laukiama klientų, šiuo atveju visi 
+      * esami sistemos IP adresai (visi interfeis'ai)
+      */
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+     servaddr.sin_port = htons(port); // nurodomas portas
+    
+    /*
+      * Serverio adresas susiejamas su socket'u
+      */
+     if (bind (l_socket, (struct sockaddr *)&servaddr,sizeof(servaddr))<0){
+        fprintf(stderr,"ERROR #3: bind listening socket.\n");
         exit(1);
     }
 
     /*
-     * Išvaloma ir užpildoma serverio adreso struktūra
-     */
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET; // nurodomas protokolas (IP)
-
-    /*
-     * Nurodomas IP adresas, kuriuo bus laukiama klientų, šiuo atveju visi
-     * esami sistemos IP adresai (visi interfeis'ai)
-     */
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(port); // nurodomas portas
-
-    /*
-     * Serverio adresas susiejamas su socket'u
-     */
-    if (bind(l_socket, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
-    {
-        fprintf(stderr, "ERROR #3: bind listening socket.\n");
+      * Nurodoma, kad socket'u l_socket bus laukiama klientų prisijungimo,
+      * eilėje ne daugiau kaip 5 aptarnavimo laukiantys klientai
+      */
+    if (listen(l_socket, 5) <0){
+        fprintf(stderr,"ERROR #4: error in listen().\n");
         exit(1);
     }
 
-    /*
-     * Nurodoma, kad socket'u l_socket bus laukiama klientų prisijungimo,
-     * eilėje ne daugiau kaip 5 aptarnavimo laukiantys klientai
-     */
-    if (listen(l_socket, 5) < 0)
-    {
-        fprintf(stderr, "ERROR #4: error in listen().\n");
-        exit(1);
-    }
-
-    pfds[0].fd = l_socket;   // server socket
-    pfds[0].events = POLLIN; // poll for incoming connections
+    pfds[0].fd = l_socket;
+    pfds[0].events = POLLIN;
 
     for (int i = 1; i <= MAX_CONNECTED_CLIENTS; i++)
     {
-        pfds[i].fd = -1; // initialize client sockets
+        pfds[i].fd = -1;
     }
 
-    for (;;)
-    { // infinite loop
-        printf("Polling...\n");
-        int activity = poll(pfds, nfds, -1);
-        if (activity < 0)
-        {
-            fprintf(stderr, "poll error\n"); // error handling
-            exit(1);
-        }
-
-        if (pfds[0].revents & POLLIN) // check if there is a new connection
-        {
-            printf("Trying to connect client.\n");
-            if ((c_socket = accept(l_socket, (struct sockaddr *)&clientaddr,
-                                   &clientaddrlen)) < 0)
+    for(;;){
+            printf("Polling...\n");
+            int activity = poll(pfds, nfds, -1);
+            if (activity < 0)
             {
-                fprintf(stderr,
-                        "ERROR #5: error occurred accepting connection.\n");
+                fprintf(stderr, "poll error");
                 exit(1);
             }
-
-            for (int i = 1; i <= MAX_CONNECTED_CLIENTS; i++) // find an empty slot for the client
+    
+            if (pfds[0].revents & POLLIN)
+            {
+                printf("Trying to connect client.\n");
+                if ((c_socket = accept(l_socket, (struct sockaddr *)&clientaddr,
+                                        &clientaddrlen)) < 0)
+                {
+                    fprintf(stderr,
+                            "ERROR #5: error occured accepting connection.\n");
+                    exit(1);
+                }
+    
+                for (int i = 1; i <= MAX_CONNECTED_CLIENTS; i++)
+                {
+                    if (pfds[i].fd == -1)
+                    {
+                        pfds[i].fd = c_socket;
+                        pfds[i].events = POLLIN;
+                        printf("Client %d fd: %d.\n", i, pfds[i].fd);
+                        break;
+                    }
+                }
+                printf("Client connected.\n");
+            }
+    
+            for (int i = 1; i <= MAX_CONNECTED_CLIENTS; i++)
             {
                 if (pfds[i].fd == -1)
+                    continue;
+    
+                if (pfds[i].revents & POLLIN)
                 {
-                    pfds[i].fd = c_socket;
-                    pfds[i].events = POLLIN;
-                    printf("Client %d fd: %d.\n", i, pfds[i].fd);
-                    break;
-                }
-            }
-            printf("Client connected.\n");
-        }
-
-        for (int i = 1; i <= MAX_CONNECTED_CLIENTS; i++) // check for incoming messages from clients
-        {
-            if (pfds[i].fd == -1) // skip empty slots
-                continue;
-
-            if (pfds[i].revents & POLLIN) // check if there is data to read
-            {
-                if (usernames[i][0] == '\0')
-                {
-                    send(pfds[i].fd, "Enter your username: ", 21, 0);
-                    s_len =
-                        recv(pfds[i].fd, usernames[i], sizeof(usernames[i]), 0);
-                    if (s_len <= 0)
+                    if (usernames[i][0] == '\0')
                     {
-                        printf("Client %s disconnected.\n", usernames[i]);
-                        close(pfds[i].fd);
-                        usernames[i][0] = '\0';
-                        pfds[i].fd = -1;
+                        send(pfds[i].fd, "Enter your username: ", 21, 0);
+                        s_len =
+                            recv(pfds[i].fd, usernames[i], sizeof(usernames[i]), 0);
+                        if (s_len <= 0)
+                        {
+                            printf("Client %s disconnected.\n", usernames[i]);
+                            close(pfds[i].fd);
+                            usernames[i][0] = '\0';
+                            pfds[i].fd = -1;
+                        }
+                        else
+                        {
+                            usernames[i][s_len] = '\0';
+                            for (int j = 0; j < s_len; j++)
+                            {
+                                if (usernames[i][j] == '\n')
+                                {
+                                    usernames[i][j] = '\0';
+                                }
+                            }
+                            printf("Client %d is now called %s.\n", i,
+                                    usernames[i]);
+                            
+                            char str[MAX_USERNAME_LENGTH + 20];
+                            sprintf(str, "%s connected.", usernames[i]);
+                            send(pfds[i].fd, "Welcome to the server!\n", 23, 0);
+                            for (int j = 1; j <= MAX_CONNECTED_CLIENTS; j++)
+                            {
+                                if (pfds[j].fd != -1 && i != j)
+                                {
+                                    send(pfds[j].fd, str, strlen(str), 0);
+                                }
+                            }
+                        }
                     }
                     else
-                    { // process the username
-                        usernames[i][s_len] = '\0';
-                        for (int j = 0; j < s_len; j++)
-                        {
-                            if (usernames[i][j] == '\n')
-                            {
-                                usernames[i][j] = '\0';
-                            }
-                        }
-                        printf("Client %d is now called %s.\n", i,
-                               usernames[i]);
-
-                        // send welcome message to the client
-
-                        char str[MAX_USERNAME_LENGTH + 20];
-                        sprintf(str, "%s connected.", usernames[i]);
-                        send(pfds[i].fd, "Welcome to the server!\n", 23, 0);
-                        for (int j = 1; j <= MAX_CONNECTED_CLIENTS; j++)
-                        {
-                            if (pfds[j].fd != -1 && i != j)
-                            {
-                                send(pfds[j].fd, str, strlen(str), 0);
-                            }
-                        }
-                    }
-                }
-                else // read messages from clients
-                {
+                    {
                     printf("Trying to read from client %d (%d).\n", i, pfds[i].fd);
                     size_t uname_length = strlen(usernames[i]);
                     strcpy(buffer, usernames[i]);
                     strcpy(buffer + uname_length, ": ");
                     s_len = recv(pfds[i].fd, buffer + (uname_length + 2),
-                                 sizeof(buffer) - (uname_length + 2), 0);
+                    sizeof(buffer) - (uname_length + 2), 0);
                     buffer[s_len + (uname_length + 2)] = '\0';
 
                     if (s_len <= 0)
@@ -386,7 +327,7 @@ int main(int argc, char *argv[])
                         usernames[i][0] = '\0';
                         pfds[i].fd = -1;
                     }
-                    else // process received messages
+                    else
                     {
                         printf("Received from client %d: \"%s\".\n", i, buffer);
                         if (buffer[uname_length + 2] == '/')
